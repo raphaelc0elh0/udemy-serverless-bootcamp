35563f48a2fc0a0dff82be836f61ddad805e7bfc
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-11-04 17:02:19 -0300

    feat: createAuction persistence with dynamodb

--- src/modules/auction/useCases/createAuction/index.ts
+++ src/modules/auction/useCases/createAuction/index.ts
@@ -1,25 +1,35 @@
 import { APIGatewayHandler } from "src/lib/api-gateway";
+import { dynamodb } from "src/lib/dynamodb";
+import { v4 as uuidV4 } from "uuid";
 
 interface IRequestBody {
   title: string;
 }
 
 type Auction = {
+  id: string;
   title: string;
   status: "OPEN" | "CLOSE";
   createdAt: string;
 };
 
-const now = new Date();
-
 const createAuction: APIGatewayHandler = async (event) => {
   const { title } = JSON.parse(event.body) as IRequestBody;
 
+  const now = new Date();
   const auction: Auction = {
+    id: uuidV4(),
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
   };
+
+  await dynamodb
+    .put({
+      TableName: "AuctionsTable",
+      Item: auction,
+    })
+    .promise();
 
   return {
     statusCode: 201,
